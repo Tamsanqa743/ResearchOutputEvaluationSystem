c1028edc59d29f49707bf7d35fd402ebd1e3e306
Tamsanqa Thwala <79663654+Tamsanqa743@users.noreply.github.com>
2026-04-22 23:42:17 +0200

    Initial class files

---
+++ evaluation_manager.py
@@ -0,0 +1,25 @@
+class evaluation_manager:
+
+    def __init__(self):
+        pass
+
+    def calaculate_average(score):
+        pass
+
+    def check_consensus():
+        pass
+
+    def apply_rules():
+        pass
+
+    def notify_acceptance():
+        pass
+
+    def notify_rejection():
+        pass
+
+    def notify_revision():
+        pass
+
+    def save_score(score):
+        pass
---
+++ notification_service.py
@@ -0,0 +1,7 @@
+class notification_service:
+
+    def __init__(self):
+        pass
+
+    def send_notification():
+        pass
